19df3a2b3133c1b786b60da160dc2ee46869946b
uttkarshy <uttkarshyadav@gmail.com>
2026-07-08 15:10:00 +0530

    Update list_basics.py

--- 04_lists/list_basics.py
+++ 04_lists/list_basics.py
@@ -1,1 +1,13 @@
+students = []
 
+students.append("Rahul")
+students.append("Utkarsh")
+students.append("Aman")
+
+print(students)
+
+students.remove("Rahul")
+
+print(students)
+
+print(len(students))
