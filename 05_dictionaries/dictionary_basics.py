28f994460436d21e4eafe32d631b8d58a347fb5f
uttkarshy <uttkarshyadav@gmail.com>
2026-07-08 15:11:23 +0530

    Update dictionary_basics.py

--- 05_dictionaries/dictionary_basics.py
+++ 05_dictionaries/dictionary_basics.py
@@ -1,1 +1,18 @@
+student = {
+    "name": "Utkarsh",
+    "marks": 95
+}
 
+print(student)
+
+student["marks"] = 100
+
+print(student)
+
+student["grade"] = "A"
+
+print(student)
+
+student.pop("grade")
+
+print(student)
