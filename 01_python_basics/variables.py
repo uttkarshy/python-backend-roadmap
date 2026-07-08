30859370e2069c29baa8f4ceca29d30d06bc24f3
uttkarshy <uttkarshyadav@gmail.com>
2026-07-08 15:04:54 +0530

    Add variable examples and reassign salary

--- 01_python_basics/variables.py
+++ 01_python_basics/variables.py
@@ -1,1 +1,21 @@
+"""
+Topic: Variables
 
+Variables store values in memory.
+
+Author: Utkarsh Yadav
+"""
+
+name = "Utkarsh"
+age = 29
+salary = 34125
+
+print("Name :", name)
+print("Age :", age)
+print("Salary :", salary)
+
+# Reassign variable
+
+salary = 40000
+
+print("Updated Salary :", salary)
